7c540a56a8ee6815a6c3cab0d5bf39a47e555dc6
Moringa School <valentinecherutich7@email.com>
2022-09-28 14:05:48 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet (someNumber) {
+  if (someNumber < 2000) {
+      return 'This one is on me!'
+  }
+  else if (someNumber < 2500) {
+      return 'I will gladly take your thirty bucks.'
+  } 
+  else (someNumber > 2500)
+      return `No can do.`
+  }
+      
+  function ternaryCheckCity(city) {
+    return ( city === `NYC` ? `Ok, sounds good.` : `No go.`);
+  }
+  
+  function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case `generous`: 
+      return `Thank you so much.`
+      break;
+    case `not as generous` :
+      return `Thank you.`
+      break;
+    default:
+      return `Bye.`
+      break;
+    }
+  }
+  
